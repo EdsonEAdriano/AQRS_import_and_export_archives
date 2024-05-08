3d8898b91f0041509e8b7f9cabf32b24d05f2e26
edson.adriano <edsoneurides0705@gmail.cpm>
2024-05-08 20:14:48 -0300

    Classe Principal Media

--- Models/Media.cs
+++ Models/Media.cs
@@ -1,7 +1,22 @@
-﻿namespace AQRS_import_and_export_archives.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AQRS_import_and_export_archives.Models
 {
     public class Media
     {
-        
+        [Key]
+        public int Id { get; set; }
+        [StringLength(30)]
+        public required string Genre { get; set; }
+        [StringLength(30)]
+        public required string Category { get; set; }
+        [StringLength(250)]
+        public required string Text { get; set; }
+        [StringLength(30)]
+        public required string Type { get; set; }
+        [StringLength(20)]
+        public required string Rating { get; set; }
+        [StringLength(50)]
+        public required string Participant { get; set; }
     }
 }
