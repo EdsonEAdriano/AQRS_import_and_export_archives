659799f4a5753445184ed55706f38656c1ee46ae
edson.adriano <edsoneurides0705@gmail.cpm>
2024-05-08 20:46:49 -0300

    Contexto do banco

---
+++ Data/ContextDbApplication.cs
@@ -0,0 +1,14 @@
+﻿using AQRS_import_and_export_archives.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AQRS_import_and_export_archives.Data
+{
+    public class ContextDbApplication : DbContext
+    {
+        public ContextDbApplication(DbContextOptions<ContextDbApplication> options) : base(options)
+        {
+        }
+
+        DbSet<Media> Medias { get; set; }
+    }
+}
