aaa41c112d4bc77de16ff6f5ae69d92e7ff8eeef
edson.adriano <edsoneurides0705@gmail.cpm>
2024-05-08 20:47:02 -0300

    Injeção de dependencias

---
+++ Data/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using AQRS_import_and_export_archives.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace AQRS_import_and_export_archives.Data
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connection = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ContextDbApplication>(options =>
+                options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 23))));
+
+            services.AddScoped<IMediaRepository, MediaRepository>();
+
+            return services;
+        }
+    }
+}
