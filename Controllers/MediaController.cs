31dbb7dde368a09efb9892487c86027a26079981
Edsu <edsoneurides0705@gmail.com>
2024-07-01 23:08:32 -0300

    Add import and export csv methods

--- Controllers/MediaController.cs
+++ Controllers/MediaController.cs
@@ -1,6 +1,8 @@
 ﻿using AQRS_import_and_export_archives.Models;
 using AQRS_import_and_export_archives.Repositories;
+using CsvHelper;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 using System.Text;
 
 namespace AQRS_import_and_export_archives.Controllers
@@ -24,8 +26,8 @@ namespace AQRS_import_and_export_archives.Controllers
                             .Get();
         }
 
-        [HttpPost("import")]
-        public async Task<ActionResult<List<Media>>> Import(IFormFile file)
+        [HttpPost("text/import")]
+        public async Task<ActionResult<List<Media>>> ImportText(IFormFile file)
         {
             var list = new List<Media>();
 
@@ -49,8 +51,8 @@ namespace AQRS_import_and_export_archives.Controllers
             return list;
         }
 
-        [HttpGet("export")]
-        public async Task<IActionResult> Export()
+        [HttpGet("text/export")]
+        public async Task<IActionResult> ExportText()
         {
             var mediaList = await _repository.Get();
 
@@ -72,6 +74,72 @@ namespace AQRS_import_and_export_archives.Controllers
 
             byte[] buffer = Encoding.UTF8.GetBytes(stringBuilder.ToString());
             return File(buffer, "text/plain", "media_data.txt");
+        }
+
+
+
+
+        [HttpPost("csv/import")]
+        public async Task<ActionResult<List<Media>>> ImportCSV(IFormFile file)
+        {
+            var medias = new List<Media>();
+
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("Invalid File.");
+            }
+
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Read();
+                csv.ReadHeader();
+
+                while (csv.Read())
+                {
+                    var media = new Media()
+                    {
+                        Genre = csv.GetField("Genre"),
+                        Category = csv.GetField("Category"),
+                        MediaName = csv.GetField("MediaName"),
+                        Type = csv.GetField("Type"),
+                        Rating = csv.GetField("Rating"),
+                        Participant = csv.GetField("Participant")
+                    };
+
+                    medias.Add(media);
+                };              
+            }
+
+            await _repository.AddRange(medias);
+            
+            return medias;
+        }
+
+        [HttpGet("csv/export")]
+        public async Task<IActionResult> Export()
+        {
+            var mediaList = await _repository.Get();
+
+            if (mediaList == null || mediaList.Count == 0)
+            {
+                return NoContent();
+            }
+
+            byte[] csvData = ConvertToCsv(mediaList);
+            return File(csvData, "text/csv", "media_data.csv");
+        }
+
+        private byte[] ConvertToCsv<T>(List<T> records)
+        {
+            using (var memoryStream = new MemoryStream())
+            using (var streamWriter = new StreamWriter(memoryStream, Encoding.UTF8))
+            using (var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
+            {
+                csvWriter.WriteRecords(records);
+                streamWriter.Flush();
+                return memoryStream.ToArray();
+            }
         }
     }
 }
